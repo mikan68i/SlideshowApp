a6367c28832cb87fc86a447896653d106ffb9d2b
高橋ひかり <takahashihikari@takahashihikarinomacbook-air.local>
2019-02-07 19:58:01 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 高橋ひかり on 2019/02/07.
+//  Copyright © 2019 高橋ひかり. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
